bfe3b5527d5db6f200fcc42a178ab6f994d4b23f
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-02-01 17:36:55 +0100

    feat: ensure authentication on dashboard page

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,4 +1,14 @@
+import {useEffect} from "react";
+import {useNavigate} from "react-router";
+import {ensureIsAuthenticated} from "./utility/ensure-is-autenticated.ts";
+
 export default function Dashboard() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        ensureIsAuthenticated(navigate);
+    }, []);
+
     return (
         <>Dashboard page</>
     );
