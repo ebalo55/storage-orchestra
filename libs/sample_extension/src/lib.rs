ec3dfb9c84c18786bf5de3bff95256bff6a58c73
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-03-01 07:50:50 +0100

    fix: sample extension update

--- libs/sample_extension/src/lib.rs
+++ libs/sample_extension/src/lib.rs
@@ -1,6 +1,5 @@
 use extensions_loader::Extension;
 use extensions_loader::tauri::AppHandle;
-use extensions_loader::tauri::async_runtime::RuntimeHandle;
 
 struct SampleExtension;
 
@@ -21,7 +20,7 @@ impl Extension for SampleExtension {
         "A sample extension for the extensions loader.".to_string()
     }
 
-    fn run(&self, _app: AppHandle, _runtime: RuntimeHandle) -> Result<(), String> {
+    fn run(&self, _app: AppHandle) -> Result<(), String> {
         println!("Sample Extension Loaded Successfully!");
         Ok(())
     }
